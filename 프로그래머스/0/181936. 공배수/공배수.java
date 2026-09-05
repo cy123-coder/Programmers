cb4a83906858bdf2ddd65a0de4a5ac74d957c876
cy123-coder <wolgugu@gmail.com>
2026-09-05 23:02:04 +0900

    Refactor solution with ternary operator example
    Added a commented-out solution using a ternary operator for clarity.

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -10,3 +10,8 @@ class Solution {
         return answer;
     }
 }
+/*class Solution {
+    public int solution(int number, int n, int m) {
+        return number % n == 0 && number % m == 0 ? 1 : 0;
+    }
+}*/ //이렇게 삼항연산자로 더 간단하게 풀이 할수 있다
