cb8b83fc84ec09e0dd8138693e4c5f2f1bfa7fbc
cy123-coder <wolgugu@gmail.com>
2026-09-05 18:41:06 +0900

    [level 0] Title: 홀짝 구분하기, Time: 131.17 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        switch(n%2){
+                case 1 -> System.out.println(n+ " is odd");
+                case 0 -> System.out.println(n + " is even");
+                default -> System.out.println ("예외");
+        }
+    }
+}
