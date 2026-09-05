e8f6d8db55b95c8ee4a912b8f6af38cf1d0e0119
cy123-coder <wolgugu@gmail.com>
2026-09-05 18:42:40 +0900

    Update 홀짝 구분하기.java

--- 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -10,5 +10,6 @@ public class Solution {
                 case 0 -> System.out.println(n + " is even");
                 default -> System.out.println ("예외");
         }
+        //System.out.print(n + " is "+(n % 2 == 0 ? "even" : "odd")); 삼항연산자를 쓸 수 있는 문제
     }
 }
