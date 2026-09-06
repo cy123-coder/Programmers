d5692b37d6352d599d1a9de1db1e22d9c92174dc
cy123-coder <wolgugu@gmail.com>
2026-09-06 22:19:34 +0900

    Update 더 크게 합치기.java

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -22,3 +22,15 @@ class Solution {
         return answer;
     }
 }
+
+/*
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        int aLong = Integer.parseInt(""+a+b);
+        int bLong = Integer.parseInt(""+b+a);
+        answer = aLong > bLong ? aLong : bLong;
+
+        return answer;
+    }
+}*/
