6745a6d44d081c93ee907f6f9b1282d464ce62ea
cy123-coder <wolgugu@gmail.com>
2026-09-17 23:56:40 +0900

    Update 조건 문자열.java

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.java
@@ -31,3 +31,19 @@ class Solution {
         return answer;
     }
 }
+/*
+
+class Solution {
+    public int solution(String ineq, String eq, int n, int m) {
+        boolean answer = false;
+        if (ineq.equals(">") && eq.equals("="))
+            answer = n >= m;
+        else if (ineq.equals("<") && eq.equals("="))
+            answer = n <= m;
+        else if (ineq.equals(">") && eq.equals("!"))
+            answer = n > m;
+        else 
+            answer = n < m;
+        return answer ? 1 : 0;
+    }
+}*/
