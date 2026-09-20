7f80d4832f9ea9f70f2f4caa687b60c55e19b37d
cy123-coder <wolgugu@gmail.com>
2026-09-20 20:50:42 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.95 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n, String control) {
+       
+        for(int i =0; i< control.length(); i++){
+             switch(control.charAt(i)){
+                     
+                     case 'w'-> n+=1;
+                     case 's'-> n-= 1;
+                     case 'd'-> n += 10;
+                     case 'a' -> n -= 10;
+                     default -> n += 0;
+}
+}
+       
+        return n;
+    }
+}
